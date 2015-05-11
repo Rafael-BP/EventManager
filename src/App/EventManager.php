cb2a8d7d993a4fd28342b405669373f28ac22a7a
Rafael-BP <rafael_bartalotti@hotmail.com>
2015-05-11 13:15:37 -0300

    Removing spaces

--- src/App/EventManager.php
+++ src/App/EventManager.php
@@ -6,7 +6,7 @@ use App\Interfaces\EventManager as EventManagerInterface;
 use App\Interfaces\Listener;
 
 class EventManager implements EventManagerInterface
-{    
+{
     /*
      * This is the events array
      * @var array
@@ -21,7 +21,7 @@ class EventManager implements EventManagerInterface
      * )
      */
     private $events;
-    
+
     /**
      * Construct
      */
@@ -29,7 +29,7 @@ class EventManager implements EventManagerInterface
     {
         $this->events = array();
     }
-    
+
     /**
      * Add a callable or listener to a new or existent event
      * @param string $eventName
@@ -39,28 +39,28 @@ class EventManager implements EventManagerInterface
     {
         switch(true) {
 	    case ( ($action instanceof callable) || ($action instanceof Listener) ):
-		
+
                 // The block below you can use for control or remove it
 	        $events = $this->getEvents();
 	        if (array_key_exists("attach", $events)) {
 	            $this->dispatch("attach", array($eventName, $action));
 	        }
-		
+
                 $this->events[$eventName][] = $action;
                 break;
             default:
                 throw new \InvalidArgumentException("Invalid type of action provided for event manager", 400);
         }
     }
-    
+
     /**
      * Dispatch event
      * @param string $eventName
      * @param array $args Optional args for event actions
      * If you event have more than one action, unless all the actions dont have
      * any args, the args array need to be a array of arrays, each index of the
-     * args array need to corresponds to the index of the action in the event 
-     * array to work properly. 
+     * args array need to corresponds to the index of the action in the event
+     * array to work properly.
      */
     public function dispatch($eventName, $args = array())
     {
@@ -91,16 +91,16 @@ class EventManager implements EventManagerInterface
         }
 
     }
-    
+
     /**
      * Return events array
      * @return array
      */
     public function getEvents()
-    {    
+    {
        return $this->events;
     }
-    
+
     /**
      * Search and return event from the events array
      * @param string $eventName
@@ -114,18 +114,18 @@ class EventManager implements EventManagerInterface
         }
         return null;
     }
-    
+
     /**
      * Remove event from events array
      * @param string $eventName
      */
     public function removeEvent($eventName)
-    {  
+    {
         $events = $this->getEvents();
         if (array_key_exists($eventName, $events)) {
            $this->events = array_splice($events, $eventName, 1);
         }
     }
-    
+
 }
 
